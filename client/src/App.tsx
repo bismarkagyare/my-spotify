4fd02ce892a0117b78e878f94dbe513dedde8667
Bismark Agyare <bismarkagyare54@gmail.com>
2024-10-21 20:47:06 +0000

    add a dashboard route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
+//import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Callback from "./components/Callback";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  
   return (
-    <div>
-      <h1>App starts here</h1>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={"/login"} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/callback" element={<Callback />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
