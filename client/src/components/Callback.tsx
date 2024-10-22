6451cc87a0d80ea3a03864c4fcd057db96b41ead
Bismark Agyare <bismarkagyare54@gmail.com>
2024-10-22 11:59:32 +0000

    use loader

--- client/src/components/Callback.tsx
+++ client/src/components/Callback.tsx
@@ -1,10 +1,9 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Callback = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -14,27 +13,31 @@ const Callback = () => {
       axios
         .get(`http://localhost:3000/callback?code=${code}`)
         .then((response) => {
-          console.log("response from server", response.data)
-          const {access_token, refresh_token} = response.data;
+          console.log("response from server", response.data);
+          const { access_token, refresh_token } = response.data;
 
-          localStorage.setItem("access_token", access_token)
-          localStorage.setItem("refresh_token", refresh_token)
+          localStorage.setItem("access_token", access_token);
+          localStorage.setItem("refresh_token", refresh_token);
 
-          navigate('/dashboard');
+          navigate("/dashboard");
         })
         .catch((error) => {
-          console.error("Error exchanging code:", error)
-          navigate('/login')
-        })
+          console.error("Error exchanging code:", error);
+          navigate("/login");
+        });
     } else {
-      navigate('/login')
+      navigate("/login");
     }
-  }, [navigate])
-
+  }, [navigate]);
 
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="flex h-screen items-center justify-center bg-white/25">
+      <div className="flex flex-col space-y-6 items-center">
+        <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-blue-600 border-t-transparent" />
+        <span className="text-center text-gray-700">loading please wait ....</span>
+      </div>
+    </div>
+  );
+};
 
-export default Callback
+export default Callback;
