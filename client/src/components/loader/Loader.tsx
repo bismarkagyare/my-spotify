5bae6045c65d5eb494eb1327ad8d52fba42955e4
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-12 13:49:07 +0000

    add loading component

---
+++ client/src/components/loader/Loader.tsx
@@ -0,0 +1,13 @@
+
+const Loader = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-white/25">
+      <div className="flex flex-col space-y-6 items-center">
+        <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-green-600 border-t-transparent" />
+        <span className="text-center text-gray-700">Authenticating with Spotify...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
