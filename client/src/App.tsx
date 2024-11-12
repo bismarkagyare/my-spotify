a34e7c6030c100f9286576a20bbc0b1b9472951b
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-12 13:47:56 +0000

    update routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,25 @@
 //import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
-import Callback from "./components/Callback";
 import Dashboard from "./components/Dashboard";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to={"/login"} />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/callback" element={<Callback />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<Navigate to={"/login"} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/callback" element={<Navigate to="/dashboard" />} />
+          </Routes>
+        </AuthContextProvider>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
