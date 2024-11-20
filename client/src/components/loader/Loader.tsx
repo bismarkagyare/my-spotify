ca6c46b5cbaa28b891efe025b6964c809d034e79
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-20 13:34:46 +0000

    use a message prop

--- client/src/components/loader/Loader.tsx
+++ client/src/components/loader/Loader.tsx
@@ -1,10 +1,9 @@
-
-const Loader = () => {
+const Loader = ({ message }: { message?: string }) => {
   return (
     <div className="flex h-screen items-center justify-center bg-white/25">
       <div className="flex flex-col space-y-6 items-center">
         <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-green-600 border-t-transparent" />
-        <span className="text-center text-gray-700">Authenticating with Spotify...</span>
+        <span className="text-center text-gray-700">{message}</span>
       </div>
     </div>
   );
