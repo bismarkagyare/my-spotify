92f32de4742a098f89ff1ffc4750c55c614f0578
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-21 16:28:11 +0000

    image types

--- client/src/types/common.ts
+++ client/src/types/common.ts
@@ -12,3 +12,17 @@ export type SideBarProps = {
   isDrawerOpen: boolean;
   setIsDrawerOpen: (isDrawerOpen: boolean) => void;
 };
+
+export interface SpotifyProfile {
+  display_name: string;
+  id: string;
+  images: { url: string }[];
+  followers: { total: number };
+  following: number;
+  playlists: number;
+}
+export interface SpotifyImage {
+  url: string;
+  height: number | null;
+  width: number | null;
+}
