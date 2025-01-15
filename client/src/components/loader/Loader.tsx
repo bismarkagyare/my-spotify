4dcca1589be331b81109516a0bb108e7fd15d49a
Bismark Agyare <bismarkagyare54@gmail.com>
2025-01-15 22:19:39 +0000

    change text color

--- client/src/components/loader/Loader.tsx
+++ client/src/components/loader/Loader.tsx
@@ -3,7 +3,7 @@ const Loader = ({ message }: { message?: string }) => {
     <div className="flex h-screen items-center justify-center bg-white/25">
       <div className="flex flex-col space-y-6 items-center">
         <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-green-600 border-t-transparent" />
-        <span className="text-center text-gray-700">{message}</span>
+        <span className="text-center text-white">{message}</span>
       </div>
     </div>
   );
